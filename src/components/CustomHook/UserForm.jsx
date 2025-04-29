b901aaf2af6793b224fe3c7af903c185bbff6cfd
Jayendra Parmar <88284160+Jp88Programmer@users.noreply.github.com>
2025-04-29 07:20:55 +0530

    Remove commit code

--- src/components/CustomHook/UserForm.jsx
+++ src/components/CustomHook/UserForm.jsx
@@ -5,7 +5,6 @@ const UserForm = () => {
   const [firstName, bindFirstName, resetFName] = useInput("");
   const [lastName, bindLastName, resetLName] = useInput("");
   let handleSubmit = (event) => {
-    // console.log(this.state)
     alert(`${firstName} and ${lastName}`);
     resetFName();
     resetLName();
